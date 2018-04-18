785189d99934c5f6613295f19473ad404056e843
johnny <yong.wei@lychr.com>
2018-04-18 10:49:29 +0800

    增加配置文件

--- configserver/src/main/java/com/lychr/configserver/ConfigServerApplication.java
+++ configserver/src/main/java/com/lychr/configserver/ConfigServerApplication.java
@@ -1,7 +1,9 @@
 package com.lychr.configserver;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @Atuthor:weiyong
@@ -10,5 +12,9 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  */
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class ConfigServerApplication {
+    public static void main(String[] args){
+        SpringApplication.run(ConfigServerApplication.class,args);
+    }
 }
